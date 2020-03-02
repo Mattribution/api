6ff4d02cfb95c0b1431a92fd721dfc86e6b9bd19
Zac Holland <zachary@pubnub.com>
2020-03-02 14:12:16 -0800

    fixed error checking for no users found for subkey

--- internal/pkg/auth0/auth0.go
+++ internal/pkg/auth0/auth0.go
@@ -21,11 +21,13 @@ func (dao *UsersDAO) FindBySecret(secret string) (*app.User, error) {
 		return nil, err
 	}
 
-	if users.Length == 0 {
+	// TODO: Make this print a 4xx error instead of flowing up to a 500
+	// this will probably involve creating a custom error object
+	if len(users.Users) == 0 {
 		return nil, errors.New("No user was found for that secret")
 	}
 
-	if users.Length > 1 {
+	if len(users.Users) > 1 {
 		errStr := "Found multiple users for one secret key"
 		// Note: This error is serious af... idk how this could happen
 		log.Println(errStr)
